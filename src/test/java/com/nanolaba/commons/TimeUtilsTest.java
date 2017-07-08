a6db1d086cdbcd4025d39d26d675143519c48644
alexander.andriishin <alexander@andriishin.com>
2017-07-08 09:51:53 +0300

    0.0.15

--- src/test/java/com/nanolaba/commons/TimeUtilsTest.java
+++ src/test/java/com/nanolaba/commons/TimeUtilsTest.java
@@ -11,15 +11,11 @@ public class TimeUtilsTest {
 
     @Test
     public void testDateInWords() {
-
-
         Assert.assertNull(TimeUtils.toStringInWords(null));
         Assert.assertEquals("первое января две тысячи тринадцатого года", TimeUtils.toStringInWords(TimeUtils.toDate("01.01.2013")));
         Assert.assertEquals("тринадцатое февраля одна тысяча семьсот двадцать третьего года", TimeUtils.toStringInWords(TimeUtils.toDate("13.02.1723")));
         Assert.assertEquals("тринадцатое февраля двухтысячного года", TimeUtils.toStringInWords(TimeUtils.toDate("13.02.2000")));
-
     }
-
 
     @Test
     public void testConvert() {
@@ -35,6 +31,20 @@ public class TimeUtilsTest {
 
         Assert.assertNotSame(date.toString(), convert.toString());
         Assert.assertEquals(date.toString(), convert2.toString());
+    }
 
+    @Test
+    public void testBusinessDays() {
+        Assert.assertEquals(0, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("08.07.2017")));
+        Assert.assertEquals(0, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("09.07.2017")));
+        Assert.assertEquals(1, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("10.07.2017")));
+        Assert.assertEquals(2, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("11.07.2017")));
+        Assert.assertEquals(3, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("12.07.2017")));
+        Assert.assertEquals(4, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("13.07.2017")));
+        Assert.assertEquals(5, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("14.07.2017")));
+        Assert.assertEquals(5, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("15.07.2017")));
+        Assert.assertEquals(5, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("16.07.2017")));
+        Assert.assertEquals(6, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("17.07.2017")));
+        Assert.assertEquals(7, TimeUtils.getWorkingDaysBetweenTwoDates(TimeUtils.toDate("08.07.2017"), TimeUtils.toDate("18.07.2017")));
     }
 }
