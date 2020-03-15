e0e3b527596e8c46be357a051131347c9cf2d542
andriishin <hZFggfwLzlrfhKFBfvsi>
2020-03-15 12:43:15 +0300

    0.0.50

---
+++ src/main/java/com/nanolaba/commons/Symbols.java
@@ -0,0 +1,31 @@
+package com.nanolaba.commons;
+
+public interface Symbols {
+
+    String ARROW_LEFT = "←"; //2190
+    String ARROW_UP = "↑"; //2191
+    String ARROW_RIGHT = "→"; //2192
+    String ARROW_DOWN = "↓"; //2193
+    String ARROW_LEFT_RIGHT = "↔"; //2194
+    String ARROW_UP_DOWN = "↕"; //2195
+    String ARROW_NORTH_WEST = "↖"; //2196
+    String ARROW_NORTH_EAST = "↗"; //2197
+    String ARROW_SOUTH_EAST = "↘"; //2198
+    String ARROW_SOUTH_WEST = "↙"; //2199
+
+    String ARROW_HEAVY_ROUND_TIPPED_RIGHT = "➜"; //279C
+    String ARROW_HEAVY_WIDE_HEADED_RIGHT = "➔"; //2794
+
+    String COPYRIGHT = "©"; //00A9
+
+    String CURRENCY_EURO = "€"; //20AC
+    String CURRENCY_DOLLAR_HEAVY = "💲"; //1F4B2
+    String CURRENCY_RUBLE = "₽"; //20BD
+    String CURRENCY_POUND = "£"; //00A3
+    String CURRENCY_POUND_HEAVY = "￡"; //FFE1
+    String CURRENCY_BITCOIN = "₿"; //20BF
+
+    String DASH_EN = "–"; //2013
+    String DASH_EM = "—"; //2014
+    String DASH_HORIZONTAL_BAR = "―"; //2015
+}
