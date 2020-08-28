3a4b800bceb8892838b526a974291acf998ea9bc
alexander.andriishin <alexander@andriishin.com>
2020-08-28 06:49:45 +0300

    0.0.54

--- src/main/java/com/nanolaba/commons/ThreadUtil.java
+++ src/main/java/com/nanolaba/commons/ThreadUtil.java
@@ -1,7 +1,11 @@
 package com.nanolaba.commons;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 public class ThreadUtil {
 
@@ -21,6 +25,28 @@ public class ThreadUtil {
             Thread.sleep(millis, nanos);
         } catch (InterruptedException e) {
             //ignore
+        }
+    }
+
+    public static String getResourceAsString(String name, Charset encoding) throws IOException {
+        try (InputStream stream = getResourceAsStream(name)) {
+            return IOUtils.toString(stream, encoding);
+        }
+    }
+
+    public static String getResourceAsString(String name, String encoding) throws IOException {
+        try (InputStream stream = getResourceAsStream(name)) {
+            return IOUtils.toString(stream, encoding);
+        }
+    }
+
+    public static String getResourceAsString(String name) throws IOException {
+        return getResourceAsString(name, Charset.defaultCharset());
+    }
+
+    public static byte[] getResourceAsByteArray(String name) throws IOException {
+        try (InputStream stream = getResourceAsStream(name)) {
+            return IOUtils.toByteArray(stream);
         }
     }
 
